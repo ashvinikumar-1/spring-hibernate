8d127fe10b7a8b8cf107c5a13552ac98aa63c3f8
ashvinikumar-1 <ashvinikumar1801@gmail.com>
2017-08-30 14:02:07 +0530

    Made changes

--- hibernate-from-scratch/src/com/ashvini/jdvc/entity/Student.java
+++ hibernate-from-scratch/src/com/ashvini/jdvc/entity/Student.java
@@ -9,13 +9,13 @@ import javax.persistence.Table;
 @Table(name = "student")
 public class Student {
 	@Id
-	@Column(name="id")
+	@Column(name = "id")
 	private int id;
-	@Column(name="first_name")
+	@Column(name = "first_name")
 	private String firstName;
-	@Column(name="last_name")
+	@Column(name = "last_name")
 	private String lastName;
-	@Column(name="email")
+	@Column(name = "email")
 	private String email;
 
 	public Student() {
