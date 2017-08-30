e695c0dd37afc716a7273e39a0eb3bafff138bbb
ashvinikumar-1 <ashvinikumar1801@gmail.com>
2017-08-30 17:28:15 +0530

    added UpdateStudent.class made changes to ReadStudent

--- hibernate-from-scratch/src/com/ashvini/jdvc/UpdateStudent.java
+++ hibernate-from-scratch/src/com/ashvini/jdvc/UpdateStudent.java
@@ -1,15 +1,12 @@
 package com.ashvini.jdvc;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.ashvini.jdvc.entity.Student;
 
-public class ReadStudent {
-
+public class UpdateStudent {
 	public static void main(String[] args) {
 		// create SessionFactory
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
@@ -22,19 +19,20 @@ public class ReadStudent {
 			session.beginTransaction();
 			// create student object
 			Student student = session.get(Student.class, 2);
-
-			System.out.println(student.getId() + " " + student.getFirstName() + " " + student.getEmail());
-			// retrieve all rows
-			List<Student> allStudent = session.createQuery("from Student s where s.id=1").getResultList();
-
-			for (Student stu : allStudent) {
-				System.out.println(stu.getId() + " " + stu.getFirstName() + " " + stu.getEmail());
-			}
+			student.setEmail("johnsnow@got.com");
+			// commit transaction
+			session.getTransaction().commit();
+			// new Session
+			session = factory.getCurrentSession();
+			// begin transaction
+			session.beginTransaction();
+			session.createQuery("Update Student s set email='john@got.com' where id=2").executeUpdate();
+			// commit transaction
+						session.getTransaction().commit();
 
 		} finally {
 			session.close();
 		}
 
 	}
-
 }
