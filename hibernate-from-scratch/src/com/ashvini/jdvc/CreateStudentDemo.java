72e644181e9c368d1403decfdcd07b561e233fda
ashvinikumar-1 <ashvinikumar1801@gmail.com>
2017-08-30 15:03:29 +0530

    created CreateStudentDemo for write data to student Table removed pool

---
+++ hibernate-from-scratch/src/com/ashvini/jdvc/CreateStudentDemo.java
@@ -0,0 +1,36 @@
+package com.ashvini.jdvc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ashvini.jdvc.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		// create SessionFactory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// create student object
+			Student student = new Student(1, "Ashvini","kumar","ashvinikumar1801@gmail.com");
+			//begin transaction
+			session.beginTransaction();
+			//save the student object
+			session.save(student);
+			//commit transaction
+			session.getTransaction().commit();
+			
+		}finally {
+			
+		}
+
+	}
+
+}
